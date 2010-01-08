69cb6b111a9c8d8656032f10d779d1966a1abf9b
Pat Lasswell <imofftoseethewizard@gmail.com>
2010-01-08 00:25:56 -0800

    added greeting, version, license

--- moosky.js
+++ moosky.js
@@ -1,6 +1,22 @@
 //=============================================================================
 //
 //
+// This file is part of Moosky.
+//
+// Moosky is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moosky is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moosky.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
 //=============================================================================
 
 
@@ -14,6 +30,22 @@ Moosky = (function ()
     return eval(Moosky.compile(Moosky.Core.read(str), env));
   };
 })();
+
+Moosky.Version = '0.1';
+Moosky.License = '\
+Moosky is free software: you can redistribute it and/or modify \n\
+it under the terms of the GNU General Public License as published by \n\
+the Free Software Foundation, either version 3 of the License, or \n\
+(at your option) any later version. \n\
+\n\
+Moosky is distributed in the hope that it will be useful, \n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of \n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n\
+GNU General Public License for more details. \n\
+\n\
+You should have received a copy of the GNU General Public License \n\
+along with Moosky.  If not, see <http://www.gnu.org/licenses/>. \n';
+
 
 //=============================================================================
 //
@@ -2369,6 +2401,18 @@ Moosky.Top = (function ()
       }
 
       return proc.apply(this, args);
+    },
+
+    greeting: function() {
+      return "Welcome to Moosky v0.1, Copyright 2010 Pat M. Lasswell.";
+    },
+
+    license: function() {
+      return Moosky.License;
+    },
+
+    version: function() {
+      return Moosky.Version;
     },
 
     gensym: (function () {
